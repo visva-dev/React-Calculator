766aceb6b1b37ad18a664910738ab7fadc046501
Visvaldas <visva.rapalis@gmail.com>
2021-02-10 09:44:36 +0200

    Change App component from function to class

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,16 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/Calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-    { Calculate }
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+}
 
 export default App;
